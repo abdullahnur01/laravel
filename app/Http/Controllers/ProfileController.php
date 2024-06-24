3904d7533de02e463bd982ed1d6c407aa537bce5
Md Nur <77728070+abdullahnur01@users.noreply.github.com>
2024-06-24 23:55:56 +0600

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,18 +6,15 @@ class ProfileController extends Controller
 {
     public function index($id)
     {
-        // Declare variables as per the question
         $name = "Donal Trump";
         $age = "75";
         
-        // Prepare data array
         $data = [
             'id' => $id,
             'name' => $name,
             'age' => $age
         ];
 
-        // Set cookie parameters
         $name = 'access_token';
         $value = '123-XYZ';
         $minutes = 1;
@@ -26,7 +23,6 @@ class ProfileController extends Controller
         $secure = false;
         $httpOnly = true;
 
-        // Return the response cookie, and status code 200
         return response()->json($data)->cookie($name, $value, $minutes, $path, $domain, $secure, $httpOnly)->setStatusCode(200);
     }
 }
